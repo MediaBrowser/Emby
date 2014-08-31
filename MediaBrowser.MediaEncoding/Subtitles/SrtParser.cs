ada5f1e0329a5c28adc7ad415d125ebcaf67bef7
Fredrik Zetterlund <fredrik.zetterlund@7illusions.com>
2014-08-31 20:01:13 +0200

    Srt parser bugfix when file contains </ font>

--- MediaBrowser.MediaEncoding/Subtitles/SrtParser.cs
+++ MediaBrowser.MediaEncoding/Subtitles/SrtParser.cs
@@ -52,7 +52,7 @@ namespace MediaBrowser.MediaEncoding.Subtitles
                     subEvent.Text = Regex.Replace(subEvent.Text, @"\{(\\[\w]+\(?([\w\d]+,?)+\)?)+\}", string.Empty, RegexOptions.IgnoreCase);
                     subEvent.Text = Regex.Replace(subEvent.Text, "<", "&lt;", RegexOptions.IgnoreCase);
                     subEvent.Text = Regex.Replace(subEvent.Text, ">", "&gt;", RegexOptions.IgnoreCase);
-                    subEvent.Text = Regex.Replace(subEvent.Text, "&lt;(\\/?(font|b|u|i|s))((\\s+(\\w|\\w[\\w\\-]*\\w)(\\s*=\\s*(?:\\\".*?\\\"|'.*?'|[^'\\\">\\s]+))?)+\\s*|\\s*)(\\/?)&gt;", "<$1$3$7>", RegexOptions.IgnoreCase);
+                    subEvent.Text = Regex.Replace(subEvent.Text, "&lt;(\\/?( font|font|b|u|i|s))((\\s+(\\w|\\w[\\w\\-]*\\w)(\\s*=\\s*(?:\\\".*?\\\"|'.*?'|[^'\\\">\\s]+))?)+\\s*|\\s*)(\\/?)&gt;", "<$1$3$7>", RegexOptions.IgnoreCase);
                     trackInfo.TrackEvents.Add(subEvent);
                 }
             }
