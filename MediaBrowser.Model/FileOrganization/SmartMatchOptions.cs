e250079ff455de7f376606cea3e2b5398985e02b
softworkz <softworkz@hotmail.com>
2015-09-23 06:12:46 +0200

    Auto-Organize: Added feature to remember/persist series matching in manual organization dialog #2
    When a filename cannot be auto-matched to an existing series name, the
    organization must be performed manually.
    Unfortunately not just once, but again and again for each episode coming
    in.
    This change proposes a simple but solid method to optionally persist the
    matching condition from within the manual organization dialog.
    This approach will make Emby "learn" how to organize files in the future
    without user interaction.

---
+++ MediaBrowser.Model/FileOrganization/SmartMatchOptions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace MediaBrowser.Model.FileOrganization
+{
+    public class SmartMatchOptions
+    {
+        /// <summary>
+        /// Gets or sets a list of smart match entries.
+        /// </summary>
+        /// <value>The smart match entries.</value>
+        public List<SmartMatchInfo> SmartMatchInfos { get; set; }
+
+        public SmartMatchOptions()
+        {
+            SmartMatchInfos = new List<SmartMatchInfo>();
+        }
+    }
+}
