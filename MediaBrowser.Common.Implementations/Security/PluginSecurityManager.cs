12af3bbdb83d515f2862d83536a926480f913307
Ashley Yakeley <ashley@yakeley.org>
2015-12-20 03:30:12 -0800

    free SupporterInfo

--- MediaBrowser.Common.Implementations/Security/PluginSecurityManager.cs
+++ MediaBrowser.Common.Implementations/Security/PluginSecurityManager.cs
@@ -160,62 +160,16 @@ namespace MediaBrowser.Common.Implementations.Security
 
         public async Task<SupporterInfo> GetSupporterInfo()
         {
-            var key = SupporterKey;
-
-            if (string.IsNullOrWhiteSpace(key))
+            return new SupporterInfo
             {
-                return new SupporterInfo();
-            }
-
-            var data = new Dictionary<string, string>
-                {
-                    { "key", key }, 
-                };
-
-            var url = MbAdmin.HttpsUrl + "/service/supporter/retrieve";
-
-            using (var stream = await _httpClient.Post(url, data, CancellationToken.None).ConfigureAwait(false))
-            {
-                var response = _jsonSerializer.DeserializeFromStream<SuppporterInfoResponse>(stream);
-
-                var info = new SupporterInfo
-                {
-                    Email = response.email,
-                    PlanType = response.planType,
-                    SupporterKey = response.supporterKey,
-                    IsActiveSupporter = IsMBSupporter
-                };
-
-                if (!string.IsNullOrWhiteSpace(response.expDate))
-                {
-                    DateTime parsedDate;
-                    if (DateTime.TryParse(response.expDate, out parsedDate))
-                    {
-                        info.ExpirationDate = parsedDate;
-                    }
-                    else
-                    {
-                        _logger.Error("Failed to parse expDate: {0}", response.expDate);
-                    }
-                }
-
-                if (!string.IsNullOrWhiteSpace(response.regDate))
-                {
-                    DateTime parsedDate;
-                    if (DateTime.TryParse(response.regDate, out parsedDate))
-                    {
-                        info.RegistrationDate = parsedDate;
-                    }
-                    else
-                    {
-                        _logger.Error("Failed to parse regDate: {0}", response.regDate);
-                    }
-                }
-
-                info.IsExpiredSupporter = info.ExpirationDate.HasValue && info.ExpirationDate < DateTime.UtcNow && !string.IsNullOrWhiteSpace(info.SupporterKey);
-
-                return info;
-            }
+                Email = "user@example.com",
+                SupporterKey = "Free Emby",
+                ExpirationDate = null,
+                RegistrationDate = new DateTime(2015,01,01),
+                PlanType = "Lifetime",
+                IsActiveSupporter = true,
+                IsExpiredSupporter = false
+            };
         }
 
         /// <summary>
