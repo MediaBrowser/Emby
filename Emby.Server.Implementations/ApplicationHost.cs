edc61079c496baeaadff3e4cd6e41b2f717d30e5
Luke Pulverenti <luke.pulverenti@gmail.com>
2018-07-20 18:28:37 -0400

    blacklist non-functional plugin

--- Emby.Server.Implementations/ApplicationHost.cs
+++ Emby.Server.Implementations/ApplicationHost.cs
@@ -1924,7 +1924,8 @@ namespace Emby.Server.Implementations
                 "MediaBrowser.Plugins.ITV.dll",
                 "MediaBrowser.Plugins.Lastfm.dll",
                 "ServerRestart.dll",
-                "MediaBrowser.Plugins.NotifyMyAndroidNotifications.dll"
+                "MediaBrowser.Plugins.NotifyMyAndroidNotifications.dll",
+                "MetadataViewer.dll"
             };
 
             return !exclude.Contains(filename ?? string.Empty, StringComparer.OrdinalIgnoreCase);
